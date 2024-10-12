0333fe0d6b070bd7de4fab0ca6100a1b2bebf7bc
Saurabh Jaiswar <155644652+Saurabhjaiswar05@users.noreply.github.com>
2024-10-12 14:34:11 +0530

    Update WorkCardData.js

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -15,7 +15,7 @@ const ProjectCardData = [
         title:"MERN_E-COMMERCE",
         text:"Thriving Facebook community and top-ranked podcast, amy inspires a grounded, tangible and self-affirmi sense of wow really can do this  for over 100,000 online enrepreneur,Best-selling marketingCourses",
         view:"https://mern-e-commerce-client-git-main-saurabhjaiswar05s-projects.vercel.app/",
-        source:"https://media.licdn.com/dms/image/D4D12AQFcFlZ_hfXRhg/article-cover_image-shrink_720_1280/0/1689299924220?e=2147483647&v=beta&t=ataqy3AquDO6DMBL_PoVAGBBhQfefPd15y9GynqYLog"
+        source:"https://github.com/Saurabhjaiswar05/MERN_E-COMMERCE_CLIENT"
      },
      {
         imgsrc:pro3,

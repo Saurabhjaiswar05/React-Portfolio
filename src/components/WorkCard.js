4a68e08e19da2b2dc6d5a5a46900b14dc49a5257
Saurabh Jaiswar <155644652+Saurabhjaiswar05@users.noreply.github.com>
2024-12-17 18:35:21 +0530

    Update WorkCard.js

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -10,8 +10,9 @@ const WorkCard = (props) => {
                 <h2 className="project-title">{ props.title}</h2>
                 <p>{ props.text}</p>
                 <div className="pro-btns">
-                    <NavLink to={props.view} className="btn">View</NavLink>
-                    <NavLink to={props.source} className="btn">Source</NavLink>
+                    <a href={props.view} target="_blank" rel="noopener noreferrer" className="btn">View</a>
+<a href={props.source} target="_blank" rel="noopener noreferrer" className="btn">Source</a>
+
                 </div>
             </div>
   )

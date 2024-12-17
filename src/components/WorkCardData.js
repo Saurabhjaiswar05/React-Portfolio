c22421f56f2dbde4d26dca81a3797ce4a3310170
Saurabh Jaiswar <155644652+Saurabhjaiswar05@users.noreply.github.com>
2024-12-17 18:28:25 +0530

    added project 4 in WorkCardData.js

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,6 +1,8 @@
 import pro1 from "../components/assets/project1.jpeg"
 import pro2 from "../components/assets/project2.jpeg"
 import pro3 from "../components/assets/project3.jpeg"
+import pro3 from "../components/assets/project4.png"
+
 
 const ProjectCardData = [
     {
@@ -23,6 +25,13 @@ const ProjectCardData = [
         text:"Thriving Facebook community and top-ranked podcast, amy inspires a grounded, tangible and self-affirmi sense of wow really can do this  for over 100,000 online enrepreneur,Best-selling marketingCourses",
         view:"/",
         source:"/"
+     },
+    {
+        imgsrc:pro4,
+        title:"Elegance Software",
+        text:"Elegance Softwares is a professional web development and design initiative that focuses on delivering responsive, dynamic, and user-friendly solutions to help businesses thrive in the digital world.",
+        view:"https://elegance-software-mu.vercel.app/",
+        source:"https://github.com/Saurabhjaiswar05/EleganceSoftware"
      }
 ]
 
